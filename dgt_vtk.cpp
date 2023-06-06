3e48fa2eecceab5d71b8ccf42322d3579b60382b
Brian Granzow <bngranz@sandia.gov>
2023-06-06 08:52:59 -0600

    Add tree debug visualization method

--- dgt_vtk.cpp
+++ dgt_vtk.cpp
@@ -257,6 +257,154 @@ void write_vtm(std::stringstream& stream, std::string const& prefix, int nblocks
   write_vtm_end(stream);
 }
 
+static void write_vtu_header(std::stringstream& out) {
+  out << "<VTKFile type=\"UnstructuredGrid\" ";
+  out << "header_type=\"UInt64\" ";
+  out << "compressor=\"vtkZLibDataCompressor\">\n";
+}
+
+static void write_tree_types(std::stringstream& stream, int dim, int num) {
+  static constexpr std::int8_t vtk_types[] = {1,3,9,12};
+  std::int8_t type = vtk_types[dim];
+  VizView<std::int8_t> types;
+  Kokkos::resize(types, num, 1);
+  for (int i = 0; i < num; ++i) {
+    types.h_view(i, 0) = type;
+  }
+  write_data_start(stream, "Int8", "types", 1);
+  write_data(stream, types, false);
+  write_data_end(stream);
+}
+
+static void write_tree_offsets(std::stringstream& stream, int num, int nents) {
+  int offset = nents;
+  VizView<int> offsets;
+  Kokkos::resize(offsets, num, 1);
+  for (int i = 0; i < num; ++i) {
+    offsets.h_view(i, 0) = offset;
+    offset += nents;
+  }
+  write_data_start(stream, "Int32", "offsets", 1);
+  write_data(stream, offsets, false);
+  write_data_end(stream);
+}
+
+static void write_tree_connectivity(std::stringstream& stream, int npoints) {
+  VizView<int> connectivity;
+  Kokkos::resize(connectivity, npoints, 1);
+  for (int i = 0; i < npoints; ++i) {
+    connectivity.h_view(i, 0) = i;
+  }
+  write_data_start(stream, "Int32", "connectivity", 1);
+  write_data(stream, connectivity, false);
+  write_data_end(stream);
+}
+
+static constexpr p3a::vector3<int> vtk_corners[] = {
+  {0,0,0},
+  {1,0,0},
+  {1,1,0},
+  {0,1,0},
+  {0,0,1},
+  {1,0,1},
+  {1,1,1},
+  {0,1,1}
+};
+
+static void write_tree_coords(
+    std::stringstream& stream,
+    Point const& base,
+    std::vector<Node*> const& leaves,
+    p3a::box3<double> const& domain,
+    int npoints,
+    int ncorners) {
+  int idx = 0;
+  VizView<double> coords;
+  Kokkos::resize(coords, npoints, DIMS);
+  for (Node* leaf : leaves) {
+    Point const pt = leaf->pt();
+    p3a::box3<double> const box = get_block_domain(base, pt, domain);
+    p3a::vector3<double> const o = box.lower();
+    p3a::vector3<double> const dx = box.extents();
+    for (int c = 0; c < ncorners; ++c) {
+      p3a::vector3<double> const x = o + hadamard_product(dx, vtk_corners[c]);
+      for  (int axis = 0;  axis < DIMS; ++axis) {
+        coords.h_view(idx, axis) = x[axis];
+      }
+      idx++;
+    }
+  }
+  write_data_start(stream, "Float64", "coordinates", DIMS);
+  write_data(stream, coords, false);
+  write_data_end(stream);
+}
+
+static void write_leaf_depths(
+    std::stringstream& stream,
+    std::vector<Node*> const& leaves) {
+  int const nleaves = leaves.size();
+  VizView<int> depths;
+  Kokkos::resize(depths, nleaves, 1);
+  for (int i = 0; i < nleaves; ++i) {
+    depths.h_view(i, 0) = leaves[i]->pt().depth;
+  }
+  write_data_start(stream, "Int32", "depth", 1);
+  write_data(stream, depths, false);
+  write_data_end(stream);
+}
+
+static void write_leaf_ijks(
+    std::stringstream& stream,
+    std::vector<Node*> const& leaves) {
+  int const nleaves = leaves.size();
+  VizView<int> ijks;
+  Kokkos::resize(ijks, nleaves, DIMS);
+  for (int i = 0; i < nleaves; ++i) {
+    p3a::vector3<int> const ijk = leaves[i]->pt().ijk;
+    for (int axis = 0; axis < DIMS; ++axis) {
+      ijks.h_view(i, axis) = ijk[axis];
+    }
+  }
+  write_data_start(stream, "Int32", "ijk", DIMS);
+  write_data(stream, ijks, false);
+  write_data_end(stream);
+}
+
+void write_tree(
+    std::filesystem::path const& path,
+    Tree& tree,
+    p3a::box3<double> const& domain) {
+  std::stringstream stream;
+  std::vector<Node*> leaves = collect_leaves(tree);
+  int const dim = tree.dim();
+  int const nleaves = leaves.size();
+  int const ncorners = ipow(2, dim);
+  int const npoints = nleaves * ncorners;
+  write_vtu_header(stream);
+  stream << "<UnstructuredGrid>\n";
+  stream << "<Piece NumberOfPoints=\"" << npoints << "\" ";
+  stream << "NumberOfCells=\"" << nleaves << "\">\n";
+  stream << "<Cells>\n";
+  write_tree_types(stream, dim, nleaves);
+  write_tree_offsets(stream, nleaves, ncorners);
+  write_tree_connectivity(stream, npoints);
+  stream << "</Cells>\n";
+  stream << "<Points>\n";
+  write_tree_coords(stream, tree.base(), leaves, domain, npoints, ncorners);
+  stream << "</Points>\n";
+  stream << "<CellData>\n";
+  write_leaf_depths(stream, leaves);
+  write_leaf_ijks(stream, leaves);
+  stream << "</CellData>\n";
+  stream << "<PointData>\n";
+  stream << "</PointData>\n";
+  stream << "</Piece>\n";
+  stream << "</UnstructuredGrid>\n";
+  stream << "</VTKFile>\n";
+  std::filesystem::path const file_path(path.string() + ".vtu");
+  write_stream(file_path, stream);
+}
+
 }
 
 }
