8ea56dc922c86c5dcceccfec324be84d402301fd
Brian Granzow <bngranz@sandia.gov>
2023-08-28 07:51:12 -0600

    Begin DG unit tests by dimension

--- test/unit/dg_dim1.cpp
+++ test/unit/dg_dim1.cpp
@@ -1,21 +1,43 @@
 #include "dg.hpp"
 
+static constexpr int dim = 1;
+
+TEST(dg_dim1, p0_q1_complete)
+{
+  test_basis(dim,0,1,false);
+}
+
+TEST(dg_dim1, p1_q2_complete)
+{
+  test_basis(dim,1,2,false);
+}
+
+TEST(dg_dim1, p2_q3_complete)
+{
+  test_basis(dim,2,3,false);
+}
+
+TEST(dg_dim1, p3_q4_complete)
+{
+  test_basis(dim,3,4,false);
+}
+
 TEST(dg_dim1, p0_q1_tensor)
 {
-  test_basis(1,0,1,true);
+  test_basis(dim,0,1,true);
 }
 
 TEST(dg_dim1, p1_q2_tensor)
 {
-  test_basis(1,1,2,true);
+  test_basis(dim,1,2,true);
 }
 
 TEST(dg_dim1, p2_q3_tensor)
 {
-  test_basis(1,2,3,true);
+  test_basis(dim,2,3,true);
 }
 
 TEST(dg_dim1, p3_q4_tensor)
 {
-  test_basis(1,3,4,true);
+  test_basis(dim,3,4,true);
 }
