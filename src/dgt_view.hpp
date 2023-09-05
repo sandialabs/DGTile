baac7dbcf63043b4e6029321163ee0e0db71b26d
Brian Granzow <bngranz@sandia.gov>
2023-09-05 09:02:59 -0600

    some ideas

--- src/dgt_view.hpp
+++ src/dgt_view.hpp
@@ -5,23 +5,48 @@
 namespace dgt {
 
 template <class T>
-using View = typename Kokkos::View<T>;
+using View = typename Kokkos::View<T, Kokkos::LayoutLeft>;
 
 template <class T>
-using HostView = typename Kokkos::View<T>::HostMirror;
-
-template <class T>
-using UnmanagedView = typename Kokkos::View<T, Kokkos::MemoryTraits<Kokkos::Unmanaged>>;
+using HostView = typename Kokkos::View<T, Kokkos::LayoutLeft>::HostMirror;
 
 #if defined(KOKKOS_ENABLE_CUDA)
+
 template <class T>
-using HostPinnedView = typename Kokkos::View<T, Kokkos::CudaHostPinnedSpace>;
+using HostPinnedView = typename Kokkos::View<T,
+      Kokkos::LayoutLeft,
+      Kokkos::CudaHostPinnedSpace>;
+
+template <class T>
+using UnmanagedView = typename Kokkos::View<T,
+      Kokkos::LayoutLeft,
+      Kokkos::CudaHostPinnedSpace,
+      Kokkos::MemoryTraits<Kokkos::Unmanaged>>;
+
 #elif defined(KOKKOS_ENABLE_HIP)
+
 template <class T>
-using HostPinnedView = typename Kokkos::View<T, Kokkos::HipHostPinnedSpace>;
+using HostPinnedView = typename Kokkos::View<T,
+      Kokkos::LayoutLeft,
+      Kokkos::HipHostPinnedSpace>;
+
+template <class T>
+using UnmanagedView = typename Kokkos::View<T,
+      Kokkos::LayoutLeft,
+      Kokkos::HipHostPinnedSpace,
+      Kokkos::MemoryTraits<Kokkos::Unmanaged>>;
+
 #else
+
 template <class T>
-using HostPinnedView = typename Kokkos::View<T>;
+using HostPinnedView = typename Kokkos::View<T,
+      Kokkos::LayoutLeft>;
+
+template <class T>
+using UnmanagedView = typename Kokkos::View<T,
+      Kokkos::LayoutLeft,
+      Kokkos::MemoryTraits<Kokkos::Unmanaged>>;
+
 #endif
 
 }
