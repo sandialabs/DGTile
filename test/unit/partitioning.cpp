0cc0f122d0941c041f45ff496f94f77ab42480e1
Brian Granzow <bngranz@sandia.gov>
2023-09-11 20:29:56 -0600

    unit: check empty ranks work with linear partitioning

--- test/unit/partitioning.cpp
+++ test/unit/partitioning.cpp
@@ -28,3 +28,22 @@ TEST(partitioning, linear_get_local_offset)
   EXPECT_EQ(get_local_offset(9,4,2), 5);
   EXPECT_EQ(get_local_offset(9,4,3), 7);
 }
+
+TEST(partitioning, check_empty_ranks_will_work)
+{
+  using namespace dgt::linear_partitioning;
+  int const num_total = 2;
+  int const num_parts = 4;
+  std::vector<int> ids{1,2};
+  EXPECT_EQ(get_local_offset(num_total, num_parts, 0), 0);
+  EXPECT_EQ(get_num_local(num_total, num_parts, 0), 1);
+  EXPECT_EQ(get_local_offset(num_total, num_parts, 1), 1);
+  EXPECT_EQ(get_num_local(num_total, num_parts, 1), 1);
+  int const part = 3;
+  int const offset = get_local_offset(num_total, num_parts, part);
+  int const num_local = get_num_local(num_total, num_parts, part);
+  auto begin = ids.begin() + offset;
+  auto end = ids.begin() + offset + num_local;
+  std::vector<int> owned_ids(begin, end);
+  EXPECT_EQ(owned_ids.size(), 0);
+}
