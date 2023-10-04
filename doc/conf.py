13c87bc49c616b803c7fbd61b4a23a0c9ceaca11
Brian Granzow <bngranz@sandia.gov>
2023-10-04 09:43:45 -0600

    doc: try it out

---
+++ doc/conf.py
@@ -0,0 +1,11 @@
+project = 'DGTile'
+copyright = 'Sandia National Laboratories'
+author = 'Sandia National Laboratories'
+release = '0.0.1'
+extensions = [
+]
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_theme = 'bizstyle'
+html_static_path = ['_static']
+html_logo = '../logo.png'
---
+++ doc/index.rst
@@ -0,0 +1,9 @@
+=======
+Welcome
+=======
+
+Welcome to the DGTile documentation.
+
+.. toctree::
+    :maxdepth: 1
+    :caption: Table of Contents:
