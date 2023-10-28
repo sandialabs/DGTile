e435dc441ff337d75ea4d334536aaef472d81936
Brian Granzow <bngranz@sandia.gov>
2023-10-27 20:30:04 -0600

    doc: switch to rtd

--- doc/conf.py
+++ doc/conf.py
@@ -6,6 +6,6 @@ extensions = [
 ]
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-html_theme = 'bizstyle'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_logo = '../logo.png'
